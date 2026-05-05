1e39d7c83a9985460bc47b9d86497b03b03f4284
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-05 12:55:12 -0700

    Fix publish config + remove macosX64

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
     kotlin("multiplatform") version "2.3.21"
@@ -44,6 +45,12 @@ kotlin {
             xcf.add(this)
         }
     }
+    macosX64 {
+        binaries.framework {
+            baseName = "Socket2"
+            xcf.add(this)
+        }
+    }
     linuxX64()
     mingwX64()
     iosArm64 {
@@ -74,6 +81,11 @@ kotlin {
         nodejs()
     }
 
+    swiftExport {
+        moduleName = "Socket2"
+        flattenPackage = "io.github.kotlinmania.socket2"
+    }
+
     android {
         namespace = "io.github.kotlinmania.socket2"
         compileSdk = 34
@@ -98,6 +110,12 @@ kotlin {
         val commonTest by getting { dependencies { implementation(kotlin("test")) } }
     }
     jvmToolchain(21)
+}
+
+rootProject.extensions.configure<YarnRootExtension>("kotlinYarn") {
+    resolution("diff", "8.0.3")
+    resolution("serialize-javascript", "7.0.5")
+    resolution("webpack", "5.106.2")
 }
 
 
