747b6fca122469f847129b4edf5e3e92a9c69993
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-05 21:56:51 -0700

    Remove duplicate Maven Central repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,6 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 dependencyResolutionManagement {
     repositories {
         google()
-        maven("https://repo1.maven.org/maven2/")
         mavenCentral()
     }
 }
