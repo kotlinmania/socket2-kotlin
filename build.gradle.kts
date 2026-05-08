c4e5544dd4533db03d2f64ddd0aba09f0fd76027
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-08 01:22:02 -0700

    Refresh project metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "io.github.kotlinmania"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 val androidSdkDir: String? =
     providers.environmentVariable("ANDROID_SDK_ROOT").orNull
